4d7e364748759ed3eaf003e8031668392471affe
Daniel Leushuis <danieleushuis@gmail.com>
2023-07-12 16:53:02 +0200

    wip

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,22 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'id.proofme.sdk.example',
-  appName: 'Proofme Example',
-  webDir: 'dist/sdk-example',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: 'id.proofme.sdk.example',
+    appName: 'Proofme Example',
+    webDir: 'dist/sdk-example',
+    server: {
+        androidScheme: 'https'
+    },
+    plugins: {
+        EpassReader: {
+            android: {
+                src: "./node_modules/@proofme-id/web/reader/android"
+            },
+            ios: {
+                src: "./node_modules/@proofme-id/web/reader/ios"
+            }
+        }
+    }
 };
 
 export default config;
