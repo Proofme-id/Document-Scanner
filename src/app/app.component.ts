43c116c760034d55f8f53e6623c42cc7844df503
Daniel Leushuis <danieleushuis@gmail.com>
2023-08-10 14:49:54 +0200

    sdk update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,6 +121,9 @@ export class AppComponent {
                 this.mrzCredentials.documentType = dg1Data.fields["documentCode"];
                 this.mrzCredentials.firstNames = dg1Data.fields["firstName"];
                 this.mrzCredentials.lastName = dg1Data.fields["lastName"];
+                this.mrzCredentials.nationality = dg1Data.fields["nationality"];
+                this.mrzCredentials.issuer = dg1Data.fields["issuingState"];
+                
                 try {
                     const imageObject = await JP2Decoder.convertJP2toJPEG({ image: base64jp2 });
                     this.passportPhoto = imageObject.image;
