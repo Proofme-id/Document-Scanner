cab039f1df06d8787e6571aaddb24de2aa3caebb
Daniel Leushuis <danieleushuis@gmail.com>
2023-10-02 17:11:35 +0200

    Feature/proof 621 document scan ios (#16)
    * WIP
    
    * For Daniel <3
    
    * wip
    
    * Cool carousel and shit
    
    * Resolve
    
    * wip
    
    * wip
    
    * fixed android + ios
    
    * bumped version
    
    * bumped
    
    * prod
    
    ---------
    
    Co-authored-by: 23Marvi <Martenschelf@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { Toast } from '@capacitor/toast';
 import { EpassReader, JP2Decoder, PassphotoScanner, Configuration } from "@proofme-id/sdk/web/reader";
 import { EDataGroup } from "@proofme-id/sdk/web/reader/enums";
@@ -17,7 +17,7 @@ import { environment } from "../environments/environment";
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     readonly TOAST_DURATION_IN_MS = 3500;
 
     iosMrzInvalidReference = this.iosMrzInvalidError.bind(this);
@@ -29,7 +29,6 @@ export class AppComponent implements OnInit {
     progress = 0;
     datagroups: INfcResult;
     readerHelper = new ReaderHelper();
-    passportPhoto: string;
 
     initialized = false;
     verified = false;
@@ -37,12 +36,15 @@ export class AppComponent implements OnInit {
     toastTimeout: NodeJS.Timeout;
     previousToast = "";
 
+    images = [];
+    imageIndex = 0;
+
     constructor(
         private ngZone: NgZone
     ) { }
 
-    ngOnInit(): void {
-        this.initializeSdk();
+    async ngOnInit(): Promise<void> {
+        await this.initializeSdk();
     }
 
     async ngOnDestroy(): Promise<void> {
@@ -52,8 +54,7 @@ export class AppComponent implements OnInit {
 
     async initializeSdk(): Promise<void> {
         if (this.initialized) {
-            await this.showToast("SDK already initialized");
-            return;
+            return await this.showToast("SDK already initialized");
         }
 
         try {
@@ -70,14 +71,12 @@ export class AppComponent implements OnInit {
 
     async mrz(): Promise<void> {
         if (!this.initialized) {
-            await this.showToast("SDK not initialized");
-            return;
+            return await this.showToast("SDK not initialized");
         }
 
         try {
+            this.resetCredentials();
             this.mrzCredentials = await EpassReader.scanMrz({ closeText: "Close" });
-            this.verified = false;
-            this.passportPhoto = "";
 
             console.log("MRZ credentials:", this.mrzCredentials);
         } catch (error) {
@@ -88,11 +87,9 @@ export class AppComponent implements OnInit {
 
     async nfc(): Promise<void> {
         if (!this.initialized) {
-            await this.showToast("SDK not initialized");
-            return;
+            return await this.showToast("SDK not initialized");
         } else if (!this.mrzCredentials) {
-            await this.showToast("Scan MRZ first");
-            return;
+            return await this.showToast("Scan MRZ first");
         }
 
         try {
@@ -103,13 +100,12 @@ export class AppComponent implements OnInit {
 
             const scanOptions: IScanOptions = {
                 documentNumber: this.mrzCredentials.documentNumber,
-                birthDate: this.mrzCredentials.birthDate,
-                expiryDate: this.mrzCredentials.expiryDate,
+                birthDate: this.mrzCredentials.birthDateDigits,
+                expiryDate: this.mrzCredentials.expiryDateDigits,
                 dataGroups: [EDataGroup.DG1, EDataGroup.DG2]
             }
             this.datagroups = await EpassReader.scanNfc(scanOptions);
             if (this.datagroups) {
-                console.log("this.datagroups:", this.datagroups);
                 delete this.datagroups.success;
 
                 const dg1Data = this.readerHelper.extractMRZFromDG1(new Uint8Array(this.datagroups.DG1));
@@ -119,8 +115,6 @@ export class AppComponent implements OnInit {
                 console.log("AppComponent - base64jp2:", base64jp2);
 
                 this.mrzCredentials.documentNumber = dg1Data.fields["documentNumber"];
-                this.mrzCredentials.birthDate = dg1Data.fields["birthDate"];
-                this.mrzCredentials.expiryDate = dg1Data.fields["expirationDate"];
                 this.mrzCredentials.gender = dg1Data.fields["sex"];
                 this.mrzCredentials.documentType = dg1Data.fields["documentCode"];
                 this.mrzCredentials.firstNames = dg1Data.fields["firstName"];
@@ -130,15 +124,14 @@ export class AppComponent implements OnInit {
 
                 try {
                     const imageObject = await JP2Decoder.convertJP2toJPEG({ image: base64jp2 });
-                    this.passportPhoto = imageObject.image;
+                    this.images.unshift(imageObject.image);
+                    console.log("Document image:", imageObject.image);
                 } catch (error) {
                     console.error(error);
                     await this.showToast("Could not parse jp2 image")
-                    this.passportPhoto = "";
                 }
 
                 this.verified = true;
-                console.log("Document image:", this.passportPhoto);
             }
         } catch (error) {
             console.error(error);
@@ -150,20 +143,65 @@ export class AppComponent implements OnInit {
 
     async passphoto(): Promise<void> {
         if (!this.initialized) {
-            await this.showToast("SDK not initialized");
-            return;
+            return await this.showToast("SDK not initialized");
+        } else if (!this.mrzCredentials) {
+            return await this.showToast("Scan MRZ first");
         }
 
         try {
             const photoScannerResult = await PassphotoScanner.scan();
             if (photoScannerResult) {
-                this.passportPhoto = photoScannerResult.face;
-                console.log("this.passportPhoto:", this.passportPhoto);
+                this.images.push(photoScannerResult.face);
             }
         } catch (error) {
             console.error(error);
-            this.showToast(error.toString());
+            await this.showToast(error.toString());
         }
+    }
+
+    async document(): Promise<void> {
+        if (!this.initialized) {
+            return await this.showToast("SDK not initialized");
+        }
+
+        try {
+            this.resetCredentials();
+            const documentInfo = await EpassReader.scanDocument({
+                translations: {
+                    frontScan: "Scan front",
+                    backScan: "Scan back",
+                    processing: "Processing...",
+                    rotate: "Please rotate the document"
+                }
+            });
+
+            if (documentInfo) {
+                this.mrzCredentials = documentInfo.mrz;
+
+                if (documentInfo.face) {
+                    this.images.push(documentInfo.face);
+                }
+
+                if (documentInfo.frontPhoto) {
+                    this.images.push(documentInfo.frontPhoto);
+                }
+
+                if (documentInfo.backPhoto) {
+                    this.images.push(documentInfo.backPhoto);
+                }
+            }
+
+            console.log("Document info:", documentInfo);
+        } catch (error) {
+            console.error(error);
+            await this.showToast(error.toString());
+        }
+    }
+
+    resetCredentials(): void {
+        this.mrzCredentials = null;
+        this.verified = false;
+        this.images = [];
     }
 
     /**
